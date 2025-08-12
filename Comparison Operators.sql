2f6c6176824a4299decd74f0c0f1e558577ed4df
md parvez alam <sr9462070@gmail.com>
2025-08-12 20:02:27 +0530

    Update Comparison Operators.sql

--- Comparison Operators.sql
+++ Comparison Operators.sql
@@ -1,3 +1,17 @@
+Sample Table – Students
+| student_id | name | marks | age | city | |------------|--------|-------|-----|---------| | 1 | Ramesh | 85 | 19 |
+Delhi | | 2 | Priya | 92 | 20 | Mumbai | | 3 | Ahmed | 67 | 18 | Delhi | | 4 | Neha | 76 | 21 | Kolkata | | 5 |
+Sameer | 58 | 19 | Mumbai | | 6 | Kiran | 89 | 22 | Delhi |
+
+Comparison Operators (6 Questions)
+1 Find students who have exactly 85 marks.
+2 List students who are not from Mumbai.
+3 Show students whose marks are greater than 80.
+4 Show students whose age is less than or equal to 19.
+5 Find students whose marks are not equal to 76.
+6 Show students whose marks are greater than or equal to 90. 
+
+  
 create database sameer7;
 use sameer7;
 create table student(
